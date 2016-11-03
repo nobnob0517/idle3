8093085d4f00af10ec73a4b9c86b51605bd0efda
nobmatsumura <akinobu.matsumura@gmail.com>
2016-11-03 13:10:16 +0900

    復元

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,25 @@
+class ProjectsController < ApplicationController
+
+  def index
+    @project = Project.includes(:user).page(params[:page]).per(5).order("created_at DESC")
+  end
+
+  def new
+  end
+
+  def create
+    Project.create(image: params[:image], title: params[:text], price: params[:price], adress: params[:adress], explain: params[:explain], user_id: current_user.id)
+  end
+
+  def destroy
+    project = Project.find(params[:id])
+    if project.user_id == current_user.id
+      project.destroy
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
--- test/controllers/projects_controller_test.rb
+++ test/controllers/projects_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class ProjectControllerTest < ActionController::TestCase
+class ProjectsControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
