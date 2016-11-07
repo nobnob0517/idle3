bc4fc94aed2b749719b918479591ff53cfeb4888
nobmatsumura <akinobu.matsumura@gmail.com>
2016-11-07 13:27:59 +0900

    no message

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,14 +1,14 @@
 class ProjectsController < ApplicationController
 
   def index
-    @projects = Project.includes(:user).page(1).per(5).order("created_at DESC")
+    @projects = Project.includes(:user).page(params[:page]).per(10).order("created_at DESC")
   end
 
   def new
   end
 
   def create
-    Project.create(image: params[:image], title: params[:text], price: params[:price], adress: params[:adress], explain: params[:explain], user_id: current_user.id)
+    Project.create(image: params[:image], title: params[:title], price: params[:price], adress: params[:adress], explain: params[:explain], user_id: current_user.id)
   end
 
   def destroy
