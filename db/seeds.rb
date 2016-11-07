94ad1a4fd8853f08bd989208455eb6b3428ef55c
nobmatsumura <akinobu.matsumura@gmail.com>
2016-11-07 11:20:02 +0900

    初期値設定

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+User.create(email: 'nob0517.development@gmail.com', password: 'Akilavinia0517', nickname: 'nob')
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
